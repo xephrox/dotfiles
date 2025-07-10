bbb19c6e248da861bf8c0e11186adec141e5ff67
xephrox <sushil.g.shenoy@gmail.com>
2025-07-10 02:30:34 +0200

    initial lazyvim config

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          auto_trigger = true,
+          keymap = {
+            accept = "<Tab>",
+            accept_word = "<M-w>",
+            accept_line = "<M-l>",
+            next = "<M-n>",
+            prev = "<M-p>",
+            dismiss = "<C-]>",
+          },
+        },
+        panel = { enabled = false },
+      })
+    end,
+  },
+
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+}
